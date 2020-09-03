239c5375425d7ede2e09eb233c85f8cbcfe4ffb5
mojitaktir <mojitaktir@gmail.com>
2020-09-03 14:47:19 +0430

    Update env.sh

--- env.sh
+++ env.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-export BOT_TOKEN='YOUR_TOKEN_HERE';
-export DB_ID='YOUR_ID_HERE';
-export DB_SECRET='YOUR_SECRET_HERE';
-export DB_TOKEN='YOUR_TOKEN_HERE';
+export BOT_TOKEN='1369233742:AAHMauBe7-TmMadbnJrlgu3BGEPPdxBc5n4';
+export DB_ID='vy689ibsfysxr2i';
+export DB_SECRET='dz4z2au6z0vwcyz';
+export DB_TOKEN='sl.AhAw0NhWstjnWkCCSHBXb-cLwJIe_ikhA1IU89pYLHaItAN7OEEUDNrMHZOJclT9pAVVUq5JbcfwreDSmX8UslRyhr381P6XF3Cp66N4ejQg9qlb0cqOZJ2-GRXNE2qqzO7RtOE';
 export NAVIGATION_ROOT='YOUR_PATH_HERE';
